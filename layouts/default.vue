0fb39b074061a06bd67d93e80e49831c6de19a3e
mac <macdev2001@gmail.com>
2023-07-16 10:36:50 +0700

    test photo page etc

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="flex">
+    <div class="flex mb-4">
       <div class="p-1 flex space-x-2 inline-block">
         <NuxtLink to="/" class="flex font-bold text-base text-center items-center p-2 bg-stone-800 rounded-lg w-cover h-10 border-2 border-zinc-500 hover:border-zinc-100">
           <img class="mr-1 w-4 h-4" src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1fa90.svg"/>Home
@@ -14,7 +14,7 @@
       </div>
 
       <div class="p-1 flex space-x-2 inline-block">
-        <NuxtLink to="/about" class="flex font-bold text-base text-center items-center p-2 bg-stone-800 rounded-lg w-fit h-10 border-2 border-zinc-500 hover:border-zinc-100">
+        <NuxtLink to="/photos" class="flex font-bold text-base text-center items-center p-2 bg-stone-800 rounded-lg w-fit h-10 border-2 border-zinc-500 hover:border-zinc-100">
           <img class="mr-1 w-4 h-4" src="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/1f4bf.svg"/>Photos
         </NuxtLink>
       </div>
---
+++ pages/photos.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+ 
+
+
+    <!-- Photo Grid -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+
+        <div class="w-full max-w-md mx-auto bg-gray-800 rounded-lg shadow-md">
+            <img src="https://mayumi.vercel.app/banner.jpg" alt="Photo 1" class="w-full h-48 object-cover rounded-t-lg">
+            <div class="">
+                <div class="p-1 mt-0.5 text-gray-200 shadow-sm">
+                    <!-- Details -->
+                    <div class="text-sm text-stone-100 p-1 rounded-md h-16 border border-gray-700 bg-gray-900">
+                        Test photo desc later
+                    </div>
+                    <div class="text-xs text-gray-500 mt-2">
+                        2023
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+
+
+ 
+
+
+  </div>
+</template>
+
+<style scoped></style>
