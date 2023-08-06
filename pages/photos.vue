381bd445f54619c5a01a405fe5b1102d1b6d4bd9
mac <bluestardotos@gmail.com>
2023-08-06 23:48:14 +0700

    fix gal styles

--- pages/photos.vue
+++ pages/photos.vue
@@ -3,7 +3,7 @@
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-2">
    <div v-for="(item, index) in PhotoData" :key="index">
     <div
-     class="w-full max-w-md mx-auto bg-white border-2 border-gray-100 dark:border-gray-700 dark:bg-zinc-800 rounded-lg shadow-lg hover:scale-105 duration-300"
+     class="w-full max-w-md mx-auto bg-white border-2 border-gray-100 dark:border-gray-700 dark:bg-zinc-800 rounded-lg shadow-lg hover:scale-105 duration-300 hover:border-teal-400"
     >
      <div class="text-base text-gray-500 dark:text-gray-300 p-1 font-bold">
       {{ item.time }}
@@ -21,7 +21,7 @@
      <div class="">
       <div class="p-1 mt-0.5 text-gray-200 shadow-sm">
        <div
-        class="text-xs text-stone-800 dark:text-stone-100 p-1 rounded-md font-semibold h-16 border border-zinc-500 bg-gray-100 dark:border-gray-700 dark:bg-stone-900 break-all font-bold"
+        class="text-xs text-stone-800 dark:text-stone-100 p-1 rounded-md font-semibold h-16 border border-zinc-500 bg-gray-100 dark:border-gray-700 dark:bg-stone-900 break-all font-bold font-mono"
        >
         {{ item.description }}
        </div>
