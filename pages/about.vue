870d4558ea1f77bd7cd53cebb382960a02fe6e48
mac <bluestardotos@gmail.com>
2023-08-08 22:15:27 +0700

    check diff

--- pages/about.vue
+++ pages/about.vue
@@ -1,50 +1,56 @@
 <template>
- <div class="container sl-1">
-  <div class="flex inline-block">
-   <h1
-    class="flex items-center hover:scale-110 duration-300 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600 text-bold"
-   >
-    Hiya
-   </h1>
-   <small>
-    <h1
-     @mouseenter="emoji = '❤️'"
-     @mouseleave="emoji = '👋'"
-     class="inline-flex"
+  <div class="sl-1 flex flex-col items-center">
+
+
+
+
+  <!--  <h1
+      class="px-4 flex items-center hover:scale-110 duration-300 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600 text-bold"
     >
-     {{ emoji }}
-    </h1>
-   </small>
-  </div>
+      Hello.
+  </h1>-->
+    
+          <!--<div class="mt-4 px-4 text-md text-slate-700 dark:text-gray-300 font-bold">
+      <div>{{ locationAndtime }}</div>
+      <div>{{ weather }}</div>
+    </div>
+	  -->
+    <div class="mt-4 px-4">
+      <p class="ml-0 w-fit border-b-2 border-b-sky-500 dark:border-teal-300 mb-4 font-mono">
+        Hey I'm
+        <button>
+          <b>
+            <span @click="toggleName">{{ name }}.</span>
+          </b>
+        </button>
+      </p>
+    </div>
 
-  <div>
-   <div class="text-md text-slate-700 dark:text-gray-300 font-bold">
-    {{ locationAndtime }}<br />{{ weather }}
-   </div>
-   <br />
-   <p class="ml-0 w-fit border-b-2 border-b-sky-500 dark:border-teal-300 mb-4 font-mono">
-    Hey I'm
-    <button>
-     <b>
-      <span @click="toggleName">{{ name }}.</span>
-     </b>
-    </button>
-   </p>
-   <p class="font-mono">
-    I am a
-    <tooltip text='<span class="text-sm">2008/04/03</span>' mode="top"
-     ><span :style="{ color: 'blue' }" v-html="age"></span></tooltip
-    >-year-old kid who loves coding and space stuff. Also i study in
-    <a href="https://en.m.wikipedia.org/wiki/Nonformal_learning"
-     >non-formal education</a
-    > untli high school.
-   </p>
+<div class="mt-4 px-4 text-md text-slate-5	00 dark:text-g
+ray-300 font-bold">
+      <div>{{ locationAndtime }}</div>
+      <div>{{ weather }}</div>
+    </div>
+
+    <div>
+
+    <p class="mt-4 font-mono px-4 text-center">
+      I am a
+      <tooltip text='<span class="text-sm">2008/04/03</span>' mode="top">
+        <span :style="{ color: 'blue' }" v-html="age"></span>
+      </tooltip>-year-old kid who loves coding and space stuff. Also I study in
+      <a class="text-blue-400 underline" href="https://en.m.wikipedia.org/wiki/Nonformal_learning"
+        >non-formal education</a
+      > until high school.
+    </p>
+    </div>
+
   </div>
- </div>
 </template>
-
 <script>
-import Tooltip from '@/components/Tooltip.vue'
+import Tooltip from '@/components/Tooltip.vue';
+
+
 
 export default {
  components: {
