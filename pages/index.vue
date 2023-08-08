e12443f6ab4396d0579bf97599c869dfac523d9a
mac <bluestardotos@gmail.com>
2023-08-08 10:50:22 +0700

    add joke sect

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
- <div class="text-center">What should i do with pages/index.vue?</div>
+
+<div class="py-20 flex justify-center items-center h-screen">
+
+  <div class="container mx-auto text-center">
+	  <h1 class="text-4xl font-bold mb-4">eLoN mUsK</h1>
+    <p class="text-lg mb-8 mx-auto">Elon Musk is an amazing guy who ruined Twitter (aka x)</p>
+    <a href="https://twitter.com/fckeveryword/status/1621296957011410944" class="bg-white text-blue-700 px-6 py-3 rounded-full font-bold hover:bg-blue-600 hover:text-white transition duration-300 ease-in-out">Fuck twitter</a>
+  </div>
+</div>
+
+
+ <div class="text-center font-mono">Note: this page is joke</div>
 </template>
 <script lang="ts">
 export default {
