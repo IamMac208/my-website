9d7fe97eccf48bbc6a6d36568e12103cc797a1c3
mac <bluestardotos@gmail.com>
2023-08-13 10:08:14 +0700

    Clean code again

--- server/api/weather.ts
+++ server/api/weather.ts
@@ -28,20 +28,4 @@ export default defineEventHandler(async () => {
  }
 })
 
-/*
-const config = useRuntimeConfig()
 
-export default defineEventHandler(async () => {
- const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${config.public.weather_location}&units=metric&appid=${config.openweathermap_key}`
- const encodedUrl = encodeURI(apiUrl)
-
- try {
-  const response = await fetch(encodedUrl)
-  const data = await response.json()
-  return data
- } catch (error) {
-  console.error('Error fetching OpenWeatherMap API:', error)
-  return null
- }
-})
-*/
